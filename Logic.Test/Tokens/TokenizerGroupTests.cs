c5c70c73a70b2856c5ad3360663c204ab1db88bc
Tatham Oddie <tatham@oddie.com.au>
2010-09-20 22:53:10 +1000

    Moved some tests into a better file.

--- Logic.Test/Tokens/TokenizerGroupTests.cs
+++ Logic.Test/Tokens/TokenizerGroupTests.cs
@@ -119,5 +119,128 @@ namespace TathamOddie.RegexAnalyzer.Logic.Test.Tokens
                 result.ToArray()
             );
         }
+
+        [TestMethod]
+        public void Tokenizer_GetTokens_ShouldTokenizeInlineComment()
+        {
+            // Arrange
+            const string input = "(?#foo!)";
+
+            // Act
+            var result = new Tokenizer(input).GetTokens();
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+                {
+                    new Token(TokenType.GroupStart, "(", 0),
+                    new Token(TokenType.GroupDirectiveStart, "?", 1),
+                    new Token(TokenType.CommentStart, "#", 2),
+                    new Token(TokenType.Literal, "f", 3),
+                    new Token(TokenType.Literal, "o", 4),
+                    new Token(TokenType.Literal, "o", 5),
+                    new Token(TokenType.Literal, "!", 6),
+                    new Token(TokenType.GroupEnd, ")", 7)
+                },
+                result.ToArray()
+            );
+        }
+
+        [TestMethod]
+        public void Tokenizer_GetTokens_ShouldTokenizePositiveLookAhead()
+        {
+            // Arrange
+            const string input = "(?=foo)";
+
+            // Act
+            var result = new Tokenizer(input).GetTokens();
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+                {
+                    new Token(TokenType.GroupStart, "(", 0),
+                    new Token(TokenType.GroupDirectiveStart, "?", 1),
+                    new Token(TokenType.PositiveLookAheadMarker, "=", 2),
+                    new Token(TokenType.Literal, "f", 3),
+                    new Token(TokenType.Literal, "o", 4),
+                    new Token(TokenType.Literal, "o", 5),
+                    new Token(TokenType.GroupEnd, ")", 6)
+                },
+                result.ToArray()
+            );
+        }
+
+        [TestMethod]
+        public void Tokenizer_GetTokens_ShouldTokenizeNegativeLookAhead()
+        {
+            // Arrange
+            const string input = "(?!foo)";
+
+            // Act
+            var result = new Tokenizer(input).GetTokens();
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+                {
+                    new Token(TokenType.GroupStart, "(", 0),
+                    new Token(TokenType.GroupDirectiveStart, "?", 1),
+                    new Token(TokenType.NegativeLookAheadMarker, "!", 2),
+                    new Token(TokenType.Literal, "f", 3),
+                    new Token(TokenType.Literal, "o", 4),
+                    new Token(TokenType.Literal, "o", 5),
+                    new Token(TokenType.GroupEnd, ")", 6)
+                },
+                result.ToArray()
+            );
+        }
+
+        [TestMethod]
+        public void Tokenizer_GetTokens_ShouldTokenizePositiveLookBehind()
+        {
+            // Arrange
+            const string input = "(?<=foo)";
+
+            // Act
+            var result = new Tokenizer(input).GetTokens();
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+                {
+                    new Token(TokenType.GroupStart, "(", 0),
+                    new Token(TokenType.GroupDirectiveStart, "?", 1),
+                    new Token(TokenType.NamedIdentifierStartOrLookBehindMarker, "<", 2),
+                    new Token(TokenType.PositiveLookBehindMarker, "=", 3),
+                    new Token(TokenType.Literal, "f", 4),
+                    new Token(TokenType.Literal, "o", 5),
+                    new Token(TokenType.Literal, "o", 6),
+                    new Token(TokenType.GroupEnd, ")", 7)
+                },
+                result.ToArray()
+            );
+        }
+
+        [TestMethod]
+        public void Tokenizer_GetTokens_ShouldTokenizeNegativeLookBehind()
+        {
+            // Arrange
+            const string input = "(?<!foo)";
+
+            // Act
+            var result = new Tokenizer(input).GetTokens();
+
+            // Assert
+            CollectionAssert.AreEqual(new[]
+                {
+                    new Token(TokenType.GroupStart, "(", 0),
+                    new Token(TokenType.GroupDirectiveStart, "?", 1),
+                    new Token(TokenType.NamedIdentifierStartOrLookBehindMarker, "<", 2),
+                    new Token(TokenType.NegativeLookBehindMarker, "!", 3),
+                    new Token(TokenType.Literal, "f", 4),
+                    new Token(TokenType.Literal, "o", 5),
+                    new Token(TokenType.Literal, "o", 6),
+                    new Token(TokenType.GroupEnd, ")", 7)
+                },
+                result.ToArray()
+            );
+        }
     }
 }

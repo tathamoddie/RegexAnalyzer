5852299076a47885476d17f6b3b3af3960b73520
Tatham Oddie <tatham@oddie.com.au>
2010-09-23 21:15:04 +1000

    Upgrade to MVC3 and added the first few views.

--- Web/Controllers/AnalysisController.cs
+++ Web/Controllers/AnalysisController.cs
@@ -1,12 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.ComponentModel;
+using System.Web.Mvc;
 
 namespace Web.Controllers
 {
     public class AnalysisController : Controller
     {
-        public string Analyze()
+        public ActionResult Analyze(
+            [DefaultValue(false)]bool verbose,
+            string q)
         {
-            return "abc";
+            if (string.IsNullOrEmpty(q))
+                return View("NoExpression");
+
+            ViewData["Expression"] = q;
+
+            return verbose ? AnalyzeVerbose(q) : AnalyzeBasic(q);
+        }
+
+        ActionResult AnalyzeBasic(string expression)
+        {
+            return View("Basic");
+        }
+
+        ActionResult AnalyzeVerbose(string expression)
+        {
+            return View("Verbose");
         }
     }
 }

67bab151df77f6b5bdfe21a055dde6cea9a30a83
Tatham Oddie <tatham@oddie.com.au>
2010-09-23 20:45:00 +1000

    Missed a file ...

---
+++ Web/Controllers/AnalysisController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class AnalysisController : Controller
+    {
+        public string Analyze()
+        {
+            return "abc";
+        }
+    }
+}

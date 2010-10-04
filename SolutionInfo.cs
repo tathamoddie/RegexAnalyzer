36cff1f47ccc69e6d7da85517ea26f94b727e97e
Tatham Oddie <tatham@oddie.com.au>
2010-10-04 22:59:08 +1100

    Implemented shared SolutionInfo.cs.

---
+++ SolutionInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Tatham Oddie")]
+[assembly: AssemblyProduct("whatdoesthisregularexpressiondo.com")]
+[assembly: AssemblyCopyright("Copyright 2010 Tatham Oddie")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
